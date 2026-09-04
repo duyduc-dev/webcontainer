c3447481e6304faa62f9554b141fe186c8b2880d
EXTERNAL Dang Duy Duc (AMARIS, MS/EET) <external.DuyDuc.Dang@bcn.bosch.com>
2026-09-04 11:07:10 +0700

    clean source code

--- packages/core/tsup.config.ts
+++ packages/core/tsup.config.ts
@@ -8,14 +8,4 @@ export default defineConfig([
     sourcemap: true,
     clean: true,
   },
-  {
-    entry: {
-      "workers/kernel/KernelWorker": "src/workers/kernel/KernelWorker.ts",
-      "workers/guest/GuestWorker": "src/workers/guest/GuestWorker.ts",
-      "workers/preview/PreviewServiceWorker": "src/workers/preview/PreviewServiceWorker.ts",
-    },
-    format: ["esm"],
-    sourcemap: true,
-    clean: false,
-  },
 ]);
